166a02e9f2aebcf98ad0ce7bc6493ec7eceae161
PietroMarilei~ <pietromrl0@gmail.com>
2023-11-30 18:54:29 +0100

    //

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();  
             $table->string('password');
             $table->string('address', 64)->nullable();
-            $table->string('phone_number', 14)->nullable();
+            $table->string('phone_number', 18)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/users_table_seeder.php
+++ database/seeders/users_table_seeder.php
@@ -8,19 +8,16 @@ use Illuminate\Database\Seeder;
 
 class users_table_seeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
         for ($i=0; $i < 10; $i++) { 
             $user = new User();
-            // $user->id();
-            $user->name = 'test';
-            $user->last_name = 'last name';
+            $user->name = 'Gino';
+            $user->last_name = 'Meccanico';
             $user->email = fake()->unique()->email();
             $user->password = 'password';
-            $user->address = '';
+            $user->address = fake()->address();
             $user->phone_number = '';
 
             $user->save();
